1df49682a65adda44a54da18f12d31432a1f9c2a
root <root@status-io.(none)>
2016-08-26 15:44:33 -0700

    Bunch of changes

---
+++ includes/oncall.php
@@ -0,0 +1,13 @@
+<h2 style="margin-left:35px; color:rgb(1, 171, 108)">Currently On Call</h2>
+<div style="width: 90%; padding: 5px; border: 5px solid rgba(208, 24, 24, 0.53); margin: 5px; margin-left:35px" class="container-fluid1">
+	<div class="row">
+		<div class="column_oncall"><h4></h4></div>
+			<span><img style="float: right; margin-right: 16px" src="/images/oncall2.png"</span>
+			
+			<div class="column_oncall">On Call Till: $sometimevariable	
+			<hr style="border-top: 2px solid rgba(1, 171, 108, 0.3);"></div>		
+			<div class="column_oncall"><h4>Contact Methods</h4></div>
+			<div class="column_oncall">Email: $someemailvariable</div>
+			<div class="column_oncall">Phone: $somephonevariable</div>
+	</div>
+</div>	
---
+++ includes/others.php
@@ -0,0 +1,11 @@
+<div style="width: 90%; padding: 10px; border: 3px solid gray; margin: 5px; margin-left:35px" class="container-fluid1"> 
+	<div class="row"> -
+		<div class="column_oncall"><h4><?php echo $memberName; ?></h4></div> 
+			<span>FROM: $fromTimeVariable  To: $toTimevariable</span>
+			<div class="column_oncall"><h4>Contact Methods</h4></div>  
+			<div class="column_oncall">Email: $someemailvariable</div>
+			<div class="column_oncall">Phone: $somephonevariable</div> 
+	</div> 
+</div>
+
+
